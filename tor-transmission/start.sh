eed6abff9a1b64c05336bea8c61a29241ad40894
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-01-14 15:43:18 -0700

    updated

--- tor-transmission/start.sh
+++ tor-transmission/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -m
+
 tor -f /etc/tor/torrc.default &
 
-torify transmission-daemon --auth --username "$TR_USER" --password "$TR_PASSWORD" --port 9091 --allowed "127.0.0.1"
+torify transmission-daemon --auth --username transmission --password transmission --port 9091 --allowed "127.0.0.1"
