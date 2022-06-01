4a13dd0fae7fa282d7c8e3e8dccff4ae2cf3747d
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-06-01 14:25:34 -0600

    fixed run.sh in wee-slack

--- wee-slack/run.sh
+++ wee-slack/run.sh
@@ -5,6 +5,6 @@ groupmod -o -g "$GID" weechat
 
 chown -R weechat:weechat /weechat
 
-[[ -e /weechat/.local/share/weechat/autoload/wee_slack.py ]] || mkdir -p /weechat/.local/share/weechat/autoload && ln -s /usr/share/weechat/python/wee_slack.py /weechat/.local/share/autoload/weechat
+[[ -e /weechat/.local/share/weechat/autoload/wee_slack.py ]] || mkdir -p /weechat/.local/share/weechat/autoload && ln -s /usr/share/weechat/python/wee_slack.py /weechat/.local/share/weechat/autoload/
 
 exec su-exec weechat /usr/bin/weechat "$@"
