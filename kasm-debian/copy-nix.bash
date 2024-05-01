d3e7545362e427be8215e00f55b3b1683521e808
Spencer Heywood <github@heywoodlh.io>
2024-05-01 08:53:22 -0600

    added kasm-debian

---
+++ kasm-debian/copy-nix.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export NIX_INSTALLED="true"
+[[ -e /nix/store ]] || export NIX_INSTALLED="false"
+
+if [[ ${NIX_INSTALLED} == "false" ]]
+then
+  echo "Nix store absent, copying from original store"
+else
+  echo "Nix is already installed."
+fi
+
+export PROFILE_INSTALLED="true"
+[[ -e /etc/profile.d/nix.sh ]] || export PROFILE_INSTALLED="false"
+
+if [[ ${PROFILE_INSTALLED} == "false" ]]
+then
+  echo "/etc/profile.d/nix.sh absent, copying original profile"
+  mkdir -p /etc/profile.d
+  cp -rn /etc/profile.d-orig/* /etc/profile.d/
+else
+  echo "Shell profile is already installed."
+fi
