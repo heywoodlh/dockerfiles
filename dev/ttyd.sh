5037d4b7d11a2f78702fa20efc4ac8a574c41c1d
Spencer Heywood <github@heywoodlh.io>
2024-12-03 17:59:19 -0700

    nerd-font-capable ttyd in dev container

--- dev/ttyd.sh
+++ dev/ttyd.sh
@@ -10,4 +10,4 @@ fi
 COMMAND="/bin/bash"
 [[ -e /home/heywoodlh/.nix-profile/bin/tmux ]] && COMMAND="/home/heywoodlh/.nix-profile/bin/tmux"
 
-ttyd -p 80 --uid 1000 --gid 1000 -w /home/heywoodlh --writable ${TTYD_ARGS} ${COMMAND}
+/bin/ttyd -W -t fontSize=16 -t fontFamily="JetBrains" -p 80 --uid 1000 --gid 1000 -w /home/heywoodlh --writable ${TTYD_ARGS} ${COMMAND}
