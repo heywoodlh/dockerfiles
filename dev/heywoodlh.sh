d69f00467db4bf550a55ad83a728d4067ed419f7
Spencer Heywood <github@heywoodlh.io>
2024-12-04 10:14:42 -0700

    using ttyd provided in /home/heywoodlh/.nix-profile/bin/ttyd if available

--- dev/heywoodlh.sh
+++ dev/heywoodlh.sh
@@ -6,6 +6,9 @@ then
     curl -H 'Cache-Control: no-cache' -L https://files.heywoodlh.io/scripts/linux.sh | bash -s -- server \
         && rm -rf /home/heywoodlh/.cache/nix/
 
+    # nerd-font ttyd
+    nix profile install "github:heywoodlh/flakes?dir=ttyd-nerd#ttyd"
+
     ## kill ttyd after tmux install
     [[ -e /home/heywoodlh/.nix-profile/bin/tmux ]] && sudo pkill -9 ttyd
 fi
--- dev/ttyd.sh
+++ dev/ttyd.sh
@@ -10,4 +10,9 @@ fi
 COMMAND="/bin/bash"
 [[ -e /home/heywoodlh/.nix-profile/bin/tmux ]] && COMMAND="/home/heywoodlh/.nix-profile/bin/tmux"
 
-/bin/ttyd -W -t fontSize=16 -t fontFamily="JetBrains" -p 80 --uid 1000 --gid 1000 -w /home/heywoodlh --writable ${TTYD_ARGS} ${COMMAND}
+if [[ -e /home/heywoodlh/.nix-profile/bin/ttyd ]]
+then
+    /home/heywoodlh/.nix-profile/bin/ttyd -W -t fontSize=16 -t fontFamily="JetBrains" -p 80 --uid 1000 --gid 1000 -w /home/heywoodlh --writable ${TTYD_ARGS} ${COMMAND}
+else
+    /usr/bin/ttyd -W -t fontSize=16 -t fontFamily="JetBrains" -p 80 --uid 1000 --gid 1000 -w /home/heywoodlh --writable ${TTYD_ARGS} ${COMMAND}
+fi
