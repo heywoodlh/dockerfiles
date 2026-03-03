b8909545f2f2aac373ffb2f88cbf8ec6d5436946
Spencer Heywood <github@heywoodlh.io>
2026-03-02 18:17:56 -0700

    tailscale-mullvad-router: exit if mullvad connection fails, updated test.sh

--- tailscale-mullvad-router/test.sh
+++ tailscale-mullvad-router/test.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -e
 onepassword_item="ohelo3flfw26d3ddenrssvdxqq"
-WIREGUARD_ENDPOINT=$(op read "op://Automation/$onepassword_item/WIREGUARD_ENDPOINT")
-WIREGUARD_ENDPOINT_PUBKEY=$(op read "op://Automation/$onepassword_item/WIREGUARD_ENDPOINT_PUBKEY")
-WIREGUARD_PRIVKEY=$(op read "op://Automation/$onepassword_item/WIREGUARD_PRIVKEY")
-WIREGUARD_ADDRESS=$(op read "op://Automation/$onepassword_item/WIREGUARD_ADDRESS")
-TS_AUTH_KEY=$(op read "op://Automation/$onepassword_item/TS_AUTH_KEY")
+WIREGUARD_ENDPOINT=$(op-wrapper read "op://Automation/$onepassword_item/WIREGUARD_ENDPOINT")
+WIREGUARD_ENDPOINT_PUBKEY=$(op-wrapper read "op://Automation/$onepassword_item/WIREGUARD_ENDPOINT_PUBKEY")
+WIREGUARD_PRIVKEY=$(op-wrapper read "op://Automation/$onepassword_item/WIREGUARD_PRIVKEY")
+WIREGUARD_ADDRESS=$(op-wrapper read "op://Automation/$onepassword_item/WIREGUARD_ADDRESS")
+TS_AUTHKEY=$(op-wrapper read "op://Automation/$onepassword_item/TS_AUTHKEY")
 
 docker volume create tailscale-test &>/dev/null || true # ignore error if exists
-docker build -t heywoodlh/tailscale-mullvad-router .
+docker build -t heywoodlh/tailscale-mullvad-router-testing .
 docker run -it --rm \
-    --privileged \
-    -v tailscale-test:/var/lib/tailscale \
-    -e WIREGUARD_ENDPOINT="${WIREGUARD_ENDPOINT}" \
-    -e WIREGUARD_ENDPOINT_PUBKEY="${WIREGUARD_ENDPOINT_PUBKEY}" \
-    -e WIREGUARD_PRIVKEY="${WIREGUARD_ENDPOINT_PUBKEY}" \
-    -e WIREGUARD_ADDRESS="${WIREGUARD_ADDRESS}" \
-    -e TS_AUTHKEY="${TS_AUTH_KEY}" \
-    heywoodlh/tailscale-mullvad-router
-
+  --privileged \
+  -v tailscale-test:/var/lib/tailscale \
+  -e WIREGUARD_ENDPOINT="${WIREGUARD_ENDPOINT}" \
+  -e WIREGUARD_ENDPOINT_PUBKEY="${WIREGUARD_ENDPOINT_PUBKEY}" \
+  -e WIREGUARD_PRIVKEY="${WIREGUARD_PRIVKEY}" \
+  -e WIREGUARD_ADDRESS="${WIREGUARD_ADDRESS}" \
+  -e TS_AUTHKEY="${TS_AUTHKEY}" \
+  -e TS_HOSTNAME="mullvad-socks-router-testing" \
+  heywoodlh/tailscale-mullvad-router-testing
