7e155fd59a0825f58e185adc2a96416b054ee4c0
Spencer Heywood <github@heywoodlh.io>
2026-03-24 16:39:42 -0600

    nix: run as nix user with UID 1000

--- nix/build.sh
+++ nix/build.sh
@@ -2,21 +2,17 @@
 # Script to test out multi-arch container build, to replicate GitHub Actions behavior
 # docker buildx behaves differently from docker build (i.e. in GitHub Actions)
 
-arch_target=("amd64" "arm64")
-root_dir=$(pwd)
-date_tag=$(date +%Y_%m_snapshot)
-
 # Create multiarch builder
-docker buildx ls | grep -q multiarch || docker buildx create --name multiarch --driver docker-container --use &>/dev/null
+docker buildx ls 2>/dev/null | grep -q multiarch || docker buildx create --name multiarch --driver docker-container --use &>/dev/null
 
 set -ex
 # Default target
-docker build --tag nix-test:latest --target=test .
-export nix_version="$(docker run -i --rm nix-test nix --version | awk '{print $3}')"
+docker build --tag heywoodlh/nix-local:latest --target=test .
+export nix_version="$(docker run -i --rm heywoodlh/nix-local:latest nix --version | rev | awk '{print $1}' | rev)"
 docker buildx build --tag docker.io/heywoodlh/nix:"${nix_version}" --tag docker.io/heywoodlh/nix:latest --platform linux/amd64,linux/arm64 --target=base .
 
 # Static target
-docker build --tag nix-test:static --target=static-test .
-export nix_static_version="$(docker run -i --rm nix-test:static nix --version | awk '{print $3}')"
+docker build --tag heywoodlh/nix-static:latest --target=static-test .
+export nix_static_version="$(docker run -i --rm heywoodlh/nix-static:latest nix --version | rev | awk '{print $1}' | rev)"
 docker buildx build --tag docker.io/heywoodlh/nix:static-"${nix_static_version}" --tag docker.io/heywoodlh/nix:static --platform linux/amd64,linux/arm64 --target=static .
 
