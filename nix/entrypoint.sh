228a6fc4a98262b27d17038b0d5790918fd2d9b9
Spencer Heywood <github@heywoodlh.io>
2026-04-05 20:04:34 -0600

    nix: copy original nix closure if /nix is empty (i.e. volume)

---
+++ nix/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# If Nix store is empty, copy it over
+if [[ ! -e /nix/store ]]
+then
+  echo "Initializing nix store"
+  sudo cp -r /opt/nix/* /nix
+  sudo chown -R 1000:1000 /nix
+fi
+
+# Remove own sudo privileges
+sudo rm /etc/sudoers.d/nix
+
+exec "${@:-bash}"
